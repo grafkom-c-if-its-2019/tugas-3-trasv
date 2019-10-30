c956918fa5cde267ff8190971eaf45385ed84d97
trasv <timotiuswishnubinsarpardede@ymail.com>
2019-10-30 17:25:25 +0700

    Update vertex.glsl

--- vertex.glsl
+++ vertex.glsl
@@ -9,9 +9,9 @@ void main() {
   fColor = vColor;
 
   mat4 RotateM = mat4(
-    cos(theta), sin(theta), 0.0, -0.6,
-    -sin(theta), cos(theta), 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0,
+    cos(theta), sin(theta), 0.0, +0.8*cos(theta) -0.7,
+    -sin(theta), cos(theta), 0.0, -0.95*sin(theta)+0.25,
+    0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
   gl_Position = vec4(vPosition, 0.0, 1.0) * RotateM;
